c2306468abd0b98c5d8bbe665ca85abb0f3f9d51
KailaMichelle <kaila22@hotmail.com>
2020-10-09 09:07:03 -0700

    updated scroll and responsiveness

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 console.log('Hello World')
 
 // NavBar 
-const navitem = document.querySelector('.navitem')
+const about = document.getElementById('navitem1')
+const skills = document.getElementById('navitem2')
+const projects = document.getElementById('navitem3')
+const contact = document.getElementById('navitem4')
+const btn = document.getElementById('menu-btn')
 
 // Event listeners
 window.addEventListener('resize', windowResize);
-navitem.addEventListener('click', collapse);
+about.addEventListener('click', collapse);
+skills.addEventListener('click', collapse);
+projects.addEventListener('click', collapse);
+contact.addEventListener('click', collapse);
 
 // Functions
 function windowResize(){
     let w = document.documentElement.clientWidth; 
-    // let h = document.documentElement.clientHeight; 
     if(w > 791) {
         let btn = document.getElementById('menu-btn')
         btn.checked = false;
@@ -20,6 +26,8 @@ function windowResize(){
 
 function collapse(){
     console.log('works')
+    console.log(btn.checked)
+    btn.checked = false;
 }
 
 
