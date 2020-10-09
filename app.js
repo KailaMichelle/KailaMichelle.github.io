9299aab746dc272b430c62668da4d5c79bfaaf84
KailaMichelle <kaila22@hotmail.com>
2020-10-08 22:14:59 -0700

    Major updates

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('Hello World')
+
+// NavBar 
+const navitem = document.querySelector('.navitem')
+
+// Event listeners
+window.addEventListener('resize', windowResize);
+navitem.addEventListener('click', collapse);
+
+// Functions
+function windowResize(){
+    let w = document.documentElement.clientWidth; 
+    // let h = document.documentElement.clientHeight; 
+    if(w > 791) {
+        let btn = document.getElementById('menu-btn')
+        btn.checked = false;
+        console.log(btn)
+    }
+}
+
+function collapse(){
+    console.log('works')
+}
+
+
+
+
